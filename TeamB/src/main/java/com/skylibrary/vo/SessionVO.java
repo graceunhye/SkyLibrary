ff19b0dcae1be458f83207b0328192992719cbed
e0161207 <e0161207@naver.com>
2021-01-19 15:47:56 +0900

    session

---
+++ TeamB/src/main/java/com/skylibrary/vo/SessionVO.java
@@ -0,0 +1,27 @@
+package com.skylibrary.vo;
+
+public class SessionVO {
+	private String userID;             
+	private String userPW;             
+	private String check;
+	
+	public String getUserID() {
+		return userID;
+	}
+	public void setUserID(String userID) {
+		this.userID = userID;
+	}
+	public String getUserPW() {
+		return userPW;
+	}
+	public void setUserPW(String userPW) {
+		this.userPW = userPW;
+	}
+	public String getCheck() {
+		return check;
+	}
+	public void setCheck(String check) {
+		this.check = check;
+	}
+	
+}
